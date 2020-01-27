ec6fa352df6629788b2033f753fb7af2e4fc70cb
wojtek2kdev <wojtek2kdev@gmail.com>
2020-01-27 18:22:59 +0100

    Add directory path interactive getter

--- utils/downloader.py
+++ utils/downloader.py
@@ -9,3 +9,11 @@ def getFilename():
     )
     return filename
 
+def getDirectory():
+    Tk().withdraw()
+    directory = filedialog.askdirectory(
+        initialdir='.',
+        title='Select destination directory for downloaded data'
+    )
+    return directory
+    
