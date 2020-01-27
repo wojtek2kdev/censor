896375782f9efad04e8ac1fc99ad9b826e59248e
wojtek2kdev <wojtek2kdev@gmail.com>
2020-01-27 18:47:12 +0100

    Add Downloader class, loading sources from csv

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,19 +1,27 @@
 from tkinter import *
 from tkinter import filedialog
 
-def getFilename():
-    Tk().withdraw()
-    filename = filedialog.askopenfilename(
-        initialdir='.',
-        title='Select file with sources'
-    )
-    return filename
+import pandas as pd
 
-def getDirectory():
-    Tk().withdraw()
-    directory = filedialog.askdirectory(
-        initialdir='.',
-        title='Select destination directory for downloaded data'
-    )
-    return directory
-    
+class Downloader:
+    def getFilename(self):
+        Tk().withdraw()
+        filename = filedialog.askopenfilename(
+            initialdir='.',
+            title='Select file with sources'
+        )
+        return filename
+
+    def getDirectory(self):
+        Tk().withdraw()
+        directory = filedialog.askdirectory(
+            initialdir='.',
+            title='Select destination directory for downloaded data'
+        )
+        return directory
+
+    def loadSources(self):
+        filename = self.getFilename()
+        sources = pd.read_csv(filename, sep=';').to_dict()
+        return sources
+
