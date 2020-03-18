ece013f4ac305d84ececf6acc6f7def920ff223a
wojtek2kdev <wojtek2kdev@gmail.com>
2020-03-19 00:29:02 +0100

    Update embedding module: Add:  - Load random background noise with specified properites Change:  - margin range

--- utils/embedding.py
+++ utils/embedding.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment as audio
 from random import randrange, sample
+from functools import reduce
 
 class Embedding:
     def __init__(self, positives, negatives, backgrounds):
@@ -9,11 +10,21 @@ class Embedding:
 
         self.negatives_range = (0,4)
         self.positives_range = (0,3)
-        self.margin_range = (500, 1500)
+        self.margin_range = (100, 1000)
         
         self.background_dBFS = -50
 
     
     
     def loadRandomBackground(self, size):
-        pass
+        path, = sample(self.backgrounds, 1)
+        _, ext = path.split('.')
+        begin = randrange(0, size)
+        
+        background = audio.from_file(path, ext)
+        background = background[begin:begin+size]
+        
+        dBFS_change = background.dBSF - self.background_dBFS
+        background = background.apply_gain(dBFS_change)
+
+        return background
