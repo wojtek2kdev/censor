b96b3bc33c9b184247b8116527c117659777d1f0
wojtek2kdev <wojtek2kdev@gmail.com>
2020-03-20 16:38:25 +0100

    Update embedding module: Add:  - load and embedding progress bars  - embedding iterations

--- utils/embedding.py
+++ utils/embedding.py
@@ -3,15 +3,17 @@ from random import randrange, sample, shuffle
 from math import ceil
 from functools import reduce
 from itertools import repeat, zip_longest
+from tqdm import tqdm
 
 class Embedding:
-    def __init__(self, positives, negatives, backgrounds, destination):
+    def __init__(self, positives, negatives, backgrounds, destination, iterations=1):
         self.positives = positives
         self.negatives = negatives
         self.backgrounds = backgrounds
         self.destination = destination
+        self.iterations = iterations
 
-        self.negatives_range = (1,4)
+        self.negatives_range = (1,5)
         self.positives_range = (1,3)
         self.margin_range = (100, 1000)
 
@@ -26,10 +28,12 @@ class Embedding:
 
     def loadTracks(self, tracks, label=0):
         loaded = []
+        bar = tqdm(total=len(tracks), desc="Load tracks")
         for path in tracks:
             _, ext = path.split('.')
             track = audio.from_file(path, ext)
             loaded.append((track, label))
+            bar.update(1)
         return loaded
 
     
@@ -95,20 +99,24 @@ class Embedding:
         positive_tracks = self.loadTracks(self.positives, 1)
         negative_tracks = self.loadTracks(self.negatives, 0)
 
-        positive_chunks = self.randomChunks(positive_tracks, self.positives_range)
-        negative_chunks = self.randomChunks(negative_tracks, self.negatives_range)
-
-        merged_chunks = [[*n, *p] for n, p in zip_longest(positive_chunks, negative_chunks, fillvalue=[])]
-
         processed = []
 
-        for i, chunk in enumerate(merged_chunks):
-            track, timestamps = self.connectTracks(chunk)
-            background = self.loadRandomBackground(len(track))
+        for i in range(self.iterations):
+            positive_chunks = self.randomChunks(positive_tracks, self.positives_range)
+            negative_chunks = self.randomChunks(negative_tracks, self.negatives_range)
+
+            merged_chunks = [[*n, *p] for n, p in zip_longest(positive_chunks, negative_chunks, fillvalue=[])]
+
+            bar = tqdm(total=len(merged_chunks), desc=f'Embedding [{i+1}/{self.iterations}]')
             
-            track_on_background = self.putOnBackground(background, track)
-            new_path = self.saveToWav(track_on_background, f'track-{i}')
-            
-            processed.append((new_path, timestamps))
+            for i, chunk in enumerate(merged_chunks):
+                track, timestamps = self.connectTracks(chunk)
+                background = self.loadRandomBackground(len(track))
+                
+                track_on_background = self.putOnBackground(background, track)
+                new_path = self.saveToWav(track_on_background, f'track-{i}')
+                
+                processed.append((new_path, timestamps))
+                bar.update(1)
 
         return processed
